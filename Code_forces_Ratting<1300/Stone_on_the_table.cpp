e72dee2c6b77419467478905c3bbd2690602cd73
Aditya KUmar <113716943+adityakumar002216@users.noreply.github.com>
2023-01-13 23:30:39 +0530

    Create Stone_on_the_table.cpp

---
+++ Code_forces_Ratting<1300/Stone_on_the_table.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+ using namespace std;
+int main () 
+{
+int n ,k=0;
+cin>>n;
+char arr[n];
+for(int i=0;i<n;i++)
+cin>>arr[i];
+for(int i=0;i<n-1;i++)
+if(arr[i]==arr[i+1])
+k++;
+cout<<k; 
+return 0;
+}
